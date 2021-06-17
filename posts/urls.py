92e0407e2b3e78dd5a915c500eee96e8426c2aca
Frank Casanova <frankcasanova.info@gmail.com>
2021-06-17 19:16:57 +0200

    routers has been added

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from .views import PostList, PostDetail, UserList, UserDetail
+from rest_framework import routers
+from rest_framework.routers import SimpleRouter
+from .views import UserViewSet, PostViewSet
 
-urlpatterns = [
-    path('<int:pk>/', PostDetail.as_view()),
-    path('', PostList.as_view()),
-    path('users/', UserList.as_view()),
-    path('users/<int:pk>/', UserDetail.as_view()),
-]
+router = SimpleRouter()
+router.register('users', UserViewSet, basename='users')
+router.register('posts', PostViewSet, basename='posts')
+
+
+urlpatterns = routers.urls
+
+# path('<int:pk>/', PostDetail.as_view()),
+# path('', PostList.as_view()),
+# path('users/', UserList.as_view()),
+# path('users/<int:pk>/', UserDetail.as_view()),
